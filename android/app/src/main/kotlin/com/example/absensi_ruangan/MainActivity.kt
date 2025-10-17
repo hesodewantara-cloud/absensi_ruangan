bfcc32a16a16790275b5902cea10e42febb9837f
hesodewantara-cloud <hesodewantara@gmail.com>
2025-10-17 08:32:03 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/absensi_ruangan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.absensi_ruangan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/build.gradle.kts
@@ -0,0 +1,25 @@
+// android/build.gradle.kts
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
